22646fe2da439a6f1687ea43b42127c66b62dbce
Felipe Batista dos Santos <felipebs779@gmail.com>
2024-06-13 15:17:23 -0300

    merge dev-felipe4

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,22 +1,24 @@
-import unittest
 import sys
 import os
+import unittest
 import tempfile
 from flask import Flask
 
+# Adicione o diretório src ao sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
+# Agora importe o app do main
 from main import app
 from data.import_data import import_csv, check_csv
 
 class DataImportTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-        self.app = True
+        self.app.testing = True
         self.upload_folder = tempfile.mkdtemp()
 
     def tearDown(self):
-        #Remotion of temporary files created during the tests
+        # Remoção de arquivos temporários criados durante os testes
         for root, dirs, files in os.walk(self.upload_folder):
             for file in files:
                 os.remove(os.path.join(root, file))
@@ -27,12 +29,10 @@ class DataImportTestCase(unittest.TestCase):
         with open(temp_csv, 'w') as f:
             f.write(csv_content)
         with open(temp_csv, 'rb') as f:
-            response = self.app.post('/upload', data={'file':f})
-        
+            response = self.app.post('/upload', data={'file': f})
+
         self.assertEqual(response.status_code, 200)
         self.assertIn('<table', response.data.decode('utf-8'))
-        print(self.assertEqual(response.status_code, 200))
-        print(self.assertIn('<table', response.data.decode('utf-8')))
 
     def test_import_csv(self):
         csv_content = "coluna1;coluna2\nvalor1;valor2"
@@ -51,10 +51,9 @@ class DataImportTestCase(unittest.TestCase):
         temp_csv = os.path.join(self.upload_folder, 'test_check.csv')
         with open(temp_csv, 'w') as f:
             f.write(csv_content)
-        
+
         content = check_csv(temp_csv)
         self.assertIn("coluna1;coluna2\nvalor1;valor2", content)
 
 if __name__ == '__main__':
     unittest.main()
-
